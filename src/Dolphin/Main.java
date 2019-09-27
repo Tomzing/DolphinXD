3592528d13c9a840b31d75eed5ea0175698072f3
Tomzing <Tomzing@users.noreply.github.com>
2019-09-27 11:55:21 +0200

    Endring i oppløsning

--- src/Dolphin/Main.java
+++ src/Dolphin/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/javafx.fxml"));
         primaryStage.setTitle("Dolphin");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
 
