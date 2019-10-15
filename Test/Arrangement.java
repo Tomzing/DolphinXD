3bd7e283e4573c7d3e989afdbb95d9cac5ee616c
SSoppen <soppenfedg@gmail.com>
2019-10-15 11:41:34 +0200

    Testen lagNyAnnetArrangement sjekker nå om objektet eksisterer

--- Test/Arrangement.java
+++ Test/Arrangement.java
@@ -1,4 +1,5 @@
 import Dolphin.Model.ArrangementAnnet;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
@@ -6,7 +7,7 @@ import java.time.format.DateTimeFormatter;
 
 public class Arrangement {
     @Test
-    void lagNyAnnetArrangement() {
+    public void lagNyAnnetArrangement() {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         LocalDateTime dateTime = LocalDateTime.parse("2002-06-28 20:15", formatter);
@@ -14,5 +15,6 @@ public class Arrangement {
         ArrangementAnnet test1 = new ArrangementAnnet ("Kult Arrangement","Ekstrem stryking",
                 1000,dateTime, dateTime, "Stedesen 8",
                 "Stryke raskt og fort med utrolige varmer!");
+        Assert.assertNotNull(test1);
     }
 }
