839b4169997be4a528a82a28051faca2bacddba9
Fredrik <soppenfedg@gmail.com>
2019-10-23 14:53:29 +0200

    Laget tester som sjekker om metoden erTall fungerer i NyBrukerKontroler

--- src/test/java/Controllers.java
+++ src/test/java/Controllers.java
@@ -1,13 +1,15 @@
+import Dolphin.Controller.NyBrukerController;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 
 class Controllers {
 
     @Test
-    public void kjorerSystemet() {
-
-
-
+    public void sjekkerFungererErTall() {
+        NyBrukerController controller1 = new NyBrukerController();
+        Assert.assertEquals("24", NyBrukerController.erTall("24") );
+        Assert.assertFalse(NyBrukerController.erTall("Hello"));
     }
 }
 
