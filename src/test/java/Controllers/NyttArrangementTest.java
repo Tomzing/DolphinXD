97b32c7185196d011de9ab15496afba3992aa185
Tomzing <Tomzing@users.noreply.github.com>
2019-11-21 21:19:55 +0100

    Laget stub av å parse en localdatetime

--- src/test/java/Controllers/NyttArrangementTest.java
+++ src/test/java/Controllers/NyttArrangementTest.java
@@ -17,81 +17,6 @@ public class NyttArrangementTest {
     private LocalDateTime fraDato = DataHandler.formaterDato("2002-06-28 20:00");
     private LocalDateTime  tilDato = DataHandler.formaterDato("2002-06-30 20:00");
 
-    private Person testbruker;
-
-    private SystemAdmin testadmin;
-
-    private Arrangement testArrangement;
-
-    private static final String filArrangementer = "src/main/resources/Database/arrangementer.csv";
-    private static final String filBrukere = "src/main/resources/Database/personer.csv";
-    private static final String filDeltagere = "src/main/resources/Database/deltagere.csv";
-    private static final String filAdministratorer = "src/main/resources/Database/administratorer.csv";
-
-    private static final String headerArrangement = "ID;NAVN;ARRANGØR;TYPE;VANSKELIGHETSGRAD;ANTALL PLASSER;PRIS;STARTTID;SLUTTID;STED;BESKRIVELSE\n";
-    private static final String headerBrukere = "FORNAVN;ETTERNAVN;ALDER;KJØNN;BRUKERNAVN;PASSORD\n";
-    private static final String headerDeltagereOgAdministratorer = "ARRANGEMENT-ID;BRUKERNAVN\n";
-
-    private static String arrangementerCSVInnhold;
-    private static String brukereCSVInnhold;
-    private static String deltagerCSVInnhold;
-    private static String administratorerCSVInnhold;
-
-    @BeforeAll
-    static void hentInnholdFraCSVFilene() {
-        arrangementerCSVInnhold = hentCSVInnhold(filArrangementer);
-        brukereCSVInnhold = hentCSVInnhold(filBrukere);
-        deltagerCSVInnhold = hentCSVInnhold(filDeltagere);
-        administratorerCSVInnhold = hentCSVInnhold(filAdministratorer);
-    }
-
-    @BeforeEach
-    void gjorKlarTest() {
-        leggInnInnholdICSVFil(filArrangementer, headerArrangement);
-        leggInnInnholdICSVFil(filBrukere, headerBrukere);
-        leggInnInnholdICSVFil(filDeltagere, headerDeltagereOgAdministratorer);
-        leggInnInnholdICSVFil(filAdministratorer, headerDeltagereOgAdministratorer);
-
-        testadmin = new SystemAdmin(1,"admintest","admintest");
-        testbruker = new Person("Test","Testesen", LocalDate.parse("2000-12-12"),"Mann","Test","test");
-        testArrangement = new Arrangement("Kult Arrangement",testbruker,"Sykkelritt",
-                "Vanskelig",1000,200, fraDato, tilDato, "Stedesen 8",
-                "Stryke raskt og fort med utrolige varmer!");
-    }
-
-    static String hentCSVInnhold(String filnavn) {
-        BufferedReader br;
-        String line;
-        StringBuilder csv = new StringBuilder();
-
-        try {
-            br = new BufferedReader(new FileReader(filnavn));
-            while ((line = br.readLine()) != null) {
-                csv.append(line).append("\n");
-            }
-            return csv.toString();
-        }
-        catch (IOException ioe) {
-            System.out.println(ioe.getMessage());
-            return "";
-        }
-    }
-
-    static void leggInnInnholdICSVFil(String filnavn, String tekst) {
-        try {
-            FileWriter filSkriver = new FileWriter(new File(filnavn));
-            BufferedWriter bufferedCsvSkriver = new BufferedWriter(filSkriver);
-
-            bufferedCsvSkriver.write(tekst);
-
-            bufferedCsvSkriver.flush();
-            bufferedCsvSkriver.close();
-        }
-        catch (IOException ioe) {
-            System.out.println(ioe.getMessage());
-        }
-    }
-
     @Test
     public void ingenTommeFelterTest() {
 
@@ -140,14 +65,5 @@ public class NyttArrangementTest {
                 sluttTidTest,
                 beskrivelseTest,
                 erTest));
-    }
-
-
-    @AfterAll
-    static void leggTilbakeCSVInnhold() {
-        leggInnInnholdICSVFil(filArrangementer, arrangementerCSVInnhold);
-        leggInnInnholdICSVFil(filBrukere, brukereCSVInnhold);
-        leggInnInnholdICSVFil(filDeltagere, deltagerCSVInnhold);
-        leggInnInnholdICSVFil(filAdministratorer, administratorerCSVInnhold);
     }
 }
